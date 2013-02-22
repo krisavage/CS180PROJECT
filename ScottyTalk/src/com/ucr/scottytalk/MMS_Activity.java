cfc5dc43e176179e3324b88c48665505418201ff
Nestor Lerena <ndlerena@gmail.com>
2013-02-22 12:21:14 -0800

    File Transfer now available, Estimated time is given for transfer file

--- ScottyTalk/src/com/ucr/scottytalk/MMS_Activity.java
+++ ScottyTalk/src/com/ucr/scottytalk/MMS_Activity.java
@@ -1,22 +1,16 @@
-package com.ucr.scottytalk;
+	package com.ucr.scottytalk;
 
+
+import java.io.File;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
-
-import com.parse.GetDataCallback;
-import com.parse.ParseException;
-import com.parse.ParseFile;
-import com.parse.ParseObject;
-import com.parse.ParsePush;
-import com.parse.SendCallback;
 
 
 public class MMS_Activity extends Activity {
@@ -37,48 +31,7 @@ public class MMS_Activity extends Activity {
 		getMenuInflater().inflate(R.menu.activity_mms_, menu);
 		return true;
 	}
-	
-	
-	void SendMMS (String Person){
-			//byte [] data = "Parth".getBytes ();
-			//ParseFile file = new ParseFile (data);
-			//file.saveInBackground ();
-		
-			 ParseFile file = new ParseFile(result.getBytes()); 
-			 file.saveInBackground(); 
-			 ParseObject object = new ParseObject("TestObject"); 
-			 object.put("file", file); 
-			 object.put("To", Person);
-			 object.saveInBackground();
-		
-		Intent sendIntent = new Intent(android.content.Intent.ACTION_SENDTO); 
-		sendIntent.putExtra("sms_body", "some text"); 
-		sendIntent.putExtra(Intent.EXTRA_STREAM, Uri.parse(result));
-		sendIntent.setType("*/*"); 
-		//sendIntent.getData();
-		startActivity(Intent.createChooser(sendIntent, "MMS to Friend"));
-		
-		
-	}
-	
-	@SuppressWarnings("null")
-	void ReceiveMMS (){
-		ParseObject MMS = null;
-		ParseFile File = (ParseFile)MMS.get("File");
-		
-		File.getDataInBackground (new GetDataCallback(){
-			public void done (byte [] data, ParseException e) {
-				if ( e == null){
-					//DO Something
-				}
-				else {
-					//something went wrong
-					e.printStackTrace();
-				}
-			}
-		
-		});
-	}
+
     
     public void Attach(View view){
     	Intent intent = new Intent (this, FileChooser.class); 
@@ -91,26 +44,19 @@ public class MMS_Activity extends Activity {
 
     	     if(resultCode == RESULT_OK){
 
-    	    	 //FILE SAVED IN RESULT
     	    	 result = data.getStringExtra("Path");
-    	       Toast.makeText(this, "File Clicked: "+result, Toast.LENGTH_SHORT).show();
-    	       String phoneNo = editText1.getText().toString();
-    	       SendMMS(phoneNo);
+
+    	    	 File file = new File(result);  
+    	    	 long size = file.length ();
+    	    	 size = ((size *8)/1000000) + 1;
+    	    	 
+    	    	 Toast.makeText(this,"Estimated Transfer Time:  " + size + "s", Toast.LENGTH_SHORT).show();
     	       
-    	       // send notifications to whoever is on channel ""
-    	       ParsePush push = new ParsePush(); 
-    	       push.setChannel("");
-    	       push.setMessage("sent from Parth computer!");
-    	       push.sendInBackground(new SendCallback() {
-    	         public void done(ParseException e) {
-    	           if (e == null) {
-    	             Log.d("push", "success!");
-    	           } else {
-    	             Log.d("push", "failure");
-    	           }
-    	         }
-    	       });
-    	 
+    	    	 Intent intent = new Intent();  
+    	    	 intent.setAction(Intent.ACTION_SEND);  
+    	    	 intent.setType("image/jpg");  
+    	    	 intent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(file) );  
+    	    	 startActivity(intent); 
     	}
     }   
 }
